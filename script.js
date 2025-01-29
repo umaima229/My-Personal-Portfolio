f4538462cc3082d6341bcd888967fbaf1603f033
Umaima Malik <umaimam309@gmail.com>
2025-01-29 00:22:58 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
